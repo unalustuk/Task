96498a1a97071376ec8c873df479f9cc1db57400
ünal <unalustk@gmail.com>
2024-09-10 18:29:25 +0300

    feat: filter posts with postId to find selected post

--- src/screens/Posts/PostDetailScreen.tsx
+++ src/screens/Posts/PostDetailScreen.tsx
@@ -1,12 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {ThunkDispatch} from '@reduxjs/toolkit';
 import React from 'react';
 interface PostScreenProps {
   route: any;
 }
 
 export default function PostDetailScreen({route}: PostScreenProps) {
+  const posts = useSelector(state => state.postsSlice);
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+
   const postId = route?.params.postId;
+
+  // find post with same postId
+  const post = posts.data.filter((item: any) => item.id === postId);
   console.log(postId);
+  console.log(post);
+
   return (
     <View>
       <Text>{postId}</Text>
