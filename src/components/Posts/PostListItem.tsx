38c892d63dc8d3dd554215a399560816ade2d809
ünal <unalustk@gmail.com>
2024-09-10 16:42:04 +0300

    feat: PostListItem component added

---
+++ src/components/Posts/PostListItem.tsx
@@ -0,0 +1,79 @@
+import {StyleSheet, Text, View, Pressable} from 'react-native';
+import React from 'react';
+import {themes} from '../../consts/styles';
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from '../../utils/metrics';
+import ArrowRight from '../../assets/icons/arrowRight.svg';
+
+interface PostListItemProps {
+  title: string;
+  body: string;
+  navigationHandler: () => void;
+}
+
+export default function PostListItem({
+  title,
+  body,
+  navigationHandler,
+}: PostListItemProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text numberOfLines={4} ellipsizeMode="tail" style={styles.body}>
+        {body}
+      </Text>
+      <Pressable
+        style={({pressed}) => [
+          {
+            opacity: pressed ? 0.6 : 1,
+          },
+          styles.bottomContainer,
+        ]}>
+        <Text style={styles.bottomContainerText}>See More</Text>
+        <ArrowRight width={horizontalScale(32)} height={horizontalScale(32)} />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: themes.colors.text.white,
+    marginVertical: verticalScale(themes.margins.small),
+    borderWidth: 1,
+    borderRadius: moderateScale(themes.radiuses.medium),
+    borderColor: themes.colors.border.theme,
+    padding: verticalScale(themes.margins.medium),
+  },
+  title: {
+    fontFamily: themes.fontFamilies.ROBOTO.bold,
+    fontSize: themes.fontSizes.medium,
+    color: themes.colors.text.primary,
+    marginBottom: verticalScale(themes.margins.large),
+  },
+  body: {
+    fontFamily: themes.fontFamilies.ROBOTO.regular,
+    lineHeight: moderateScale(20),
+    fontSize: themes.fontSizes.medium,
+    color: themes.colors.text.primary,
+    marginBottom: verticalScale(themes.margins.large),
+    marginRight: horizontalScale(themes.margins.small),
+  },
+
+  bottomContainer: {
+    flexDirection: 'row',
+    alignContent: 'flex-start',
+    justifyContent: 'center',
+  },
+  bottomContainerText: {
+    marginLeft: 'auto',
+    marginRight: horizontalScale(themes.margins.large),
+    fontFamily: themes.fontFamilies.ROBOTO.medium,
+    textAlignVertical: 'center',
+    fontSize: themes.fontSizes.medium,
+    color: themes.colors.text.primary,
+  },
+});
--- src/screens/Posts/PostsScreen.tsx
+++ src/screens/Posts/PostsScreen.tsx
@@ -1,12 +1,25 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {themes} from '../../consts/styles';
+import PostListItem from '../../components/Posts/PostListItem';
+import {horizontalScale} from '../../utils/metrics';
 
 export default function PostsScreen() {
   return (
-    <View>
-      <Text>PostsScreen</Text>
+    <View style={styles.container}>
+      <PostListItem
+        title="POSTTITLE"
+        body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themes.colors.text.white,
+    paddingHorizontal: horizontalScale(themes.paddings.xlarge),
+    paddingVertical: horizontalScale(themes.paddings.xlarge),
+  },
+});
